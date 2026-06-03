aaab1313eb3e3ab7ec56ba49e0254ed22b6c1ced
onoevotingsurvey2026-ctrl <onoe.voting.survey2026@gmail.com>
2026-06-03 19:53:32 +0530

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,73 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
+const app = initializeApp({
+databaseURL:"https://tnmnp-membership-system-default-rtdb.firebaseio.com"
+});
+
+const db = getDatabase(app);
+
+let DATA = [];
+
+/* LOAD */
+function load(){
+
+onValue(ref(db,"members"), snap=>{
+
+DATA = [];
+
+snap.forEach(s=>{
+DATA.push(s.val());
+});
+
+render(DATA);
+
+});
+
+}
+load();
+
+/* RENDER */
+function render(list){
+
+table.innerHTML = list.map(d=>`
+<tr>
+<td>${d.id}</td>
+<td>${d.name}</td>
+<td>${d.mobile}</td>
+<td>${d.email}</td>
+</tr>
+`).join("");
+
+count.innerText = list.length;
+}
+
+/* SEARCH */
+window.filter = function(){
+
+let v = search.value.toLowerCase();
+
+let f = DATA.filter(d =>
+(d.name||"").toLowerCase().includes(v) ||
+(d.mobile||"").includes(v)
+);
+
+render(f);
+};
+
+/* CSV */
+window.exportCSV = function(){
+
+let csv="ID,Name,Mobile,Email\n";
+
+DATA.forEach(d=>{
+csv+=`${d.id},${d.name},${d.mobile},${d.email}\n`;
+});
+
+let blob = new Blob([csv]);
+let a = document.createElement("a");
+
+a.href = URL.createObjectURL(blob);
+a.download = "TMNP.csv";
+a.click();
+};
